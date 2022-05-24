2f197ddd5bb83cc81ed59aba06243ddbccb95a47
Rafael <rboccunigodfrey@gmail.cm>
2022-05-24 12:53:32 -0400

    Fixes + license

--- src/neural_evolution/core.clj
+++ src/neural_evolution/core.clj
@@ -1,6 +1,31 @@
 ; Adaptation of David Randall Miller's evolutionary program by Rafael Boccuni-Godfrey and Aryan Raval
 ; https://www.youtube.com/watch?v=N3tRFayqVtk
 
+;
+; MIT License
+;
+; Copyright (c) 2021-2022 David R. Miller and other contributors. For the
+; exact contribution history, see the revision logs at the project page
+; https://github.com/davidrmiller/biosim4.
+;
+; Permission is hereby granted, free of charge, to any person obtaining a copy
+; of this software and associated documentation files (the "Software"), to deal
+; in the Software without restriction, including without limitation the rights
+; to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+; copies of the Software, and to permit persons to whom the Software is
+; furnished to do so, subject to the following conditions:
+;
+; The above copyright notice and this permission notice shall be included in all
+; copies or substantial portions of the Software.
+;
+; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+; IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+; FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+; AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+; LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+; OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+; SOFTWARE.
+
 (ns neural-evolution.core
   (:use [neural-evolution.views.textual]
         [neural-evolution.views.visual]))
--- src/neural_evolution/data/synapse_v2.clj
+++ src/neural_evolution/data/synapse_v2.clj
@@ -44,3 +44,108 @@
 
 #_(defn make-syn-nodes [syn-vec]
   (mapv #(gen-syn-node-vec % syn-vec) syn-vec))
+
+
+
+
+
+
+
+(defn neural-paths [ind population objects pheromones food]
+  (let [syn-vec (filter-dup-synapses (:neural-map ind))
+        get-parents (fn [child]
+                      (filterv #(= (:sink-neuron %)
+                                   (:source-neuron child))
+                               syn-vec))
+        get-children (fn [parent]
+                       (filterv #(= (:sink-neuron parent)
+                                    (:source-neuron %))
+                                syn-vec))
+        get-value (fn [syn]
+                    (if (contains? sensory-neuron-functions (:source-neuron syn))
+                      (((:source-neuron syn) sensory-neuron-functions) ind population objects pheromones food)
+                      ((:source-neuron syn) internal-neurons)))
+        origin-syn-vec (filterv #(empty? (get-parents %)) syn-vec)
+        add-to-path (fn [path syn-vec]
+                      (conj
+                        path
+                        (if (in? origin-syn-vec syn-vec)
+                          [(:source-neuron syn-vec) (:weight syn-vec) (get-value syn-vec) (:sink-neuron syn-vec)]
+                          [(:source-neuron syn-vec) (:weight syn-vec) (:sink-neuron syn-vec)])))]
+    (println (apply str (interleave (sort-by :sink-neuron syn-vec) (repeat "\r\n"))))
+    (mapv
+      (fn [origin-syn]
+        (letfn [(recur-syn [motor-outputs cur-syn depth]
+                  (if (> depth 50)
+                    (conj motor-outputs "error")
+                    (if (contains? motor-neuron-functions (:sink-neuron cur-syn))
+                      (add-to-path motor-outputs cur-syn)
+                      (mapv
+                        #(recur-syn
+                           (conj
+                             motor-outputs
+                             (add-to-path motor-outputs cur-syn)
+                             #_(if (in? origin-syn-vec %)
+                                 [(:source-neuron cur-syn) (get-value cur-syn) (:weight cur-syn)]
+                                 [(:source-neuron cur-syn) (:weight cur-syn)]))
+                           %
+                           (inc depth))
+                        (get-children cur-syn)))))]
+          (recur-syn [] origin-syn 0))) origin-syn-vec)))
+
+(defn collate-values [ind population objects pheromones food]
+  (let [neural-vals (distinct
+                      (filter
+                        not-empty
+                        (flatten-btm-lvl (neural-paths ind population objects pheromones food))))]
+    (loop [motor-vals {} known-vals {} remaining neural-vals]
+      (if (= (count (filter #(contains? motor-neuron-functions
+                                        (:sink-neuron %)) (:neural-map ind))) (count motor-vals))
+        motor-vals
+        (if (contains? motor-neuron-functions (last (first remaining)))
+          (if (= 4 (count (first remaining)))
+            (recur (assoc motor-vals
+                     (last (first remaining))
+                     (if (contains? motor-vals (last (first remaining)))
+                       (tanh (+ (tanh (apply * (take 2 (rest (first remaining)))))
+                                ((last (first remaining)) motor-vals)))
+                       (tanh (apply * (take 2 (rest (first remaining)))))))
+                   known-vals
+                   (rest remaining))
+            (if (every? #(contains? known-vals (first %))
+                        (filter #(= (first (first remaining))
+                                    (last %)) neural-vals))
+              (recur (assoc motor-vals
+                       (last (first remaining))
+                       (reduce
+                         #(tanh (+ %1 %2))
+                         (mapv #(* ((first %) known-vals)
+                                   (second (first remaining)))
+                               (filter #(= (first (first remaining))
+                                           (last %)) neural-vals))))
+                     known-vals
+                     (rest remaining))
+              (recur motor-vals known-vals (conj (rest remaining) (first remaining)))))
+          (if (= 4 (count (first remaining)))
+            (recur motor-vals
+                   (assoc known-vals
+                     (first (first remaining))
+                     (if (contains? known-vals (first (first remaining)))
+                       (tanh (+ (tanh (apply * (take 2 (rest (first remaining)))))
+                                ((first (first remaining)) known-vals)))
+                       (tanh (apply * (take 2 (rest (first remaining)))))))
+                   (rest remaining))
+            (if (every? #(contains? known-vals (first %))
+                        (filter #(= (first (first remaining))
+                                    (last %)) neural-vals))
+              (recur motor-vals
+                     (assoc known-vals
+                       (first (first remaining))
+                       (reduce
+                         #(tanh (+ %1 %2))
+                         (mapv #(* ((first %) known-vals)
+                                   (second (first remaining)))
+                               (filter #(= (first (first remaining))
+                                           (last %)) neural-vals))))
+                     (rest remaining))
+              (recur motor-vals known-vals (conj (rest remaining) (first remaining))))))))))
